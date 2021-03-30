d0e732e9848df04a69eb980d24584f4a98d98092
Nokaji <71786224+Nokaji@users.noreply.github.com>
2021-03-30 17:50:47 +0200

    add fontawesome

---
+++ app/assets/js/scripts/swinginterface.js
@@ -0,0 +1,33 @@
+/**
+ * Paladium Launcher - https://github.com/Chaika9/paladiumlauncher
+ * Copyright (C) 2019 Paladium
+ */
+
+ let currentView;
+
+ const VIEWS = {
+     login: '#login-view',
+     launcher: '#launcher-view',
+ }
+ 
+ function switchView(current, next, onNextFade = () => {}) {
+     currentView = next;
+     $(`${current}`).hide();
+     
+     $(`${next}`).fadeIn(500, () => {
+         onNextFade();
+     });
+ }
+ 
+ function getCurrentView() {
+     return currentView;
+ }
+ 
+ function showMainUI(view) {
+     setTimeout(() => {
+         $('#main').show();
+ 
+         currentView = view;
+         $(view).fadeIn(1000);
+     }, 750);
+ }
