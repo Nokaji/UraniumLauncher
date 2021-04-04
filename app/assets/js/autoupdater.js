7414d0b43ef0d6600d8957e2ede380468df1b338
Nokaji <71786224+Nokaji@users.noreply.github.com>
2021-04-04 17:01:54 +0200

    update

--- app/assets/js/autoupdater.js
+++ app/assets/js/autoupdater.js
@@ -3,6 +3,8 @@ const axios = require('axios');
 
 const electron = require('electron');
 
+const 
+
 const os = require('os');
 
 var version = ("./app/assets/distri.json");
@@ -12,22 +14,26 @@ let versions = JSON.parse(rawdata);
 
 let forceUpdate = false;
 
-VersionLauncher = versions.launcher.version;
+VersionLauncher = (versions.launcher.version);
 
 console.log("Actuel Version : " + VersionLauncher);
 
 function updaterVerify(){
+    document.getElementById("version-actuel").style.visibility = "hidden";
+    document.getElementById("version-none-actuel").style.visibility = "hidden";
     //Version Récente
     axios.get("https://beta-uranium.yvleis.fr/ressources/download/launcher/sources/distri.json")
     .then(response => {
-        VersionLauncherMin = response.data.launcher.version;
+        VersionLauncherMin = (response.data.launcher.version);
         console.log("Récents version " + VersionLauncherMin);
         if(VersionLauncherMin == VersionLauncher){
             console.log("Le Launcher est à jour(Version : " + VersionLauncher + ")");
             document.getElementById("version-actuel").style.visibility = "visible";
+            document.getElementById("version-none-actuel").style.visibility = "hidden";
         }else{
             document.getElementById("version-none-actuel").style.visibility = "visible";
-            /*let forceUpdate = true;
+            document.getElementById("version-actuel").style.visibility = "hidden";
+            let forceUpdate = true;
             var fileexe;
             if(process.platform === 'darwin') {
                 fileexe = '.dmg';
@@ -45,7 +51,7 @@ function updaterVerify(){
                         properties: {directory: electron.remote.app.getPath("temp")}
                     });
                 }
-            }*/
+            }
         }
     });
 }
--- app/assets/js/scripts/settings/launcher-version.js
+++ app/assets/js/scripts/settings/launcher-version.js
@@ -1,7 +1,5 @@
 function initSettingsLauncherVersionTab() {
-    const selectedAcc = ConfigManager.getSelectedAccount();
     document.getElementById('version-launcher-value').innerHTML = VersionLauncher;
-    updaterVerifyactual-version-launcher
 }
 
 $("#actuel-version-launcher").click(function() {
